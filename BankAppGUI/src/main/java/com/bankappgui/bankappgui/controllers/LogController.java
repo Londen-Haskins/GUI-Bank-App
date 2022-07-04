0dbafdf28e6778a6dce503338c958f6184132275
Londen <56968907+Londen-Haskins@users.noreply.github.com>
2022-07-04 18:11:34 -0400

    Fixed transaction log function
    Changed the private log variables to e initialized using 'FXCollections' instead of creating an interface

--- BankAppGUI/src/main/java/com/bankappgui/bankappgui/controllers/LogController.java
+++ BankAppGUI/src/main/java/com/bankappgui/bankappgui/controllers/LogController.java
@@ -5,6 +5,8 @@ import com.bankappgui.bankappgui.View;
 import com.bankappgui.bankappgui.ViewSwitch;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
@@ -12,23 +14,26 @@ import javafx.scene.control.ScrollPane;
 
 import java.io.*;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import javax.swing.*;
 
 public class LogController {
 
     @FXML private ListView listView;
+    ObservableList<String> transactionLog = FXCollections.observableArrayList();
 
     // Grab a reference to the GlobalData singleton
     private GlobalData globalData = GlobalData.getInstance();
 
     @FXML
     public void initialize(){
-        ObservableList<String> transactionLog = globalData.getLog().getLog();
+        transactionLog = globalData.getLog().getLog();
         listView.setItems(transactionLog);
         globalData.getLog().showLog();
+    }
+
+    public void makeReport() throws IOException {
+        globalData.getLog().printLog();
     }
 
     public void toMenu() throws IOException {
